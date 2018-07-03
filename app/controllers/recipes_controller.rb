adc74dcad3b9b52c6e8d3f237dc56d28e50a7dae
Evangelista, Abreu <rafael.evans@live.com>
2018-07-03 19:18:06 -0300

    Usuário cadastra suas receitas

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,4 +3,21 @@ class RecipesController < ApplicationController
     id = params[:id]
     @recipe = Recipe.find(id)
   end
+
+  def new
+    @recipe = Recipe.new()
+  end
+
+  def create
+    recipe = Recipe.create(recipe_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :recipe_type, :cuisine, :difficulty, :cook_time, :ingredients, :cook_method)
+
+  end
+
 end
