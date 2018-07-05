32e310ec86a6709fce94436dd4f3c08de8f05fe0
Evangelista, Abreu <rafael.evans@live.com>
2018-07-04 21:00:02 -0300

    Visitante filtra receitas por cozinha

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -2,5 +2,9 @@ class CuisinesController < ApplicationController
   def show
     id = params[:id]
     @cuisine = Cuisine.find(id)
-  end
+
+    if @cuisine.recipes.length == 0
+      flash[:warning] = 'Nenhuma receita encontrada para este tipo de cozinha'
+    end
+  end 
 end
